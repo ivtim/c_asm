7dc00bf276ebe3cc4a8562ccadaf0d0388764ffc
ivtim <53061328+ivtim@users.noreply.github.com>
2021-04-25 14:55:03 +0300

    Update elements_sum.asm

--- elements_sum.asm
+++ elements_sum.asm
@@ -2,19 +2,19 @@ SECTION .TEXT
 	GLOBAL elements_sum
 
 elements_sum:
-    push rbp
-    mov rbp, rsp
-    push rbx
-    
-    xor ecx, ecx
-    xor eax, eax
-    
-    sloop:
-        add eax, [4 * rcx + rdi]
-        inc ecx
-        cmp ecx, esi
-        jb sloop
-    
+	push rbp
+	mov rbp, rsp
+	push rbx
+
+	xor ecx, ecx
+	xor eax, eax
+
+	sloop:
+		add eax, [4 * rcx + rdi]
+		inc ecx
+		cmp ecx, esi
+		jb sloop
+
 	pop rbx
 	pop rbp                
-    ret                     
+	ret                     
